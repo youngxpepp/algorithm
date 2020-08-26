75cfc4e25a6329893fb63e9aba0fce80974d873f
youngxpepp <youngxpepp@gmail.com>
2020-08-26 11:38:05 +0900

    solve programmers 60060

--- src/programmers/60060/main.cpp
+++ src/programmers/60060/main.cpp
@@ -1,63 +1,165 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <string>
+#include <set>
 #include <map>
 
 using namespace std;
 
-bool queryWords(const string &word, const string &query) {
-    if(word.size() != query.size()) {
-        return false;
+class TrieNode {
+public:
+    bool terminal = false;
+    bool word = false;
+    vector<TrieNode*> children;
+    map<int, int> wordLengthMap;
+
+    TrieNode() {
+        children.assign(26, nullptr);
     }
 
-    if(query.front() == '?' && query.back() == '?') {
-        return true;
-    } else if(query.front() == '?') {
-        const int lastIndex = query.find_last_of('?');
-
-        for(int i = lastIndex + 1; i < word.size(); i++) {
-            if(word[i] != query[i]) {
-                return false;
-            }
-        }
-        return true;
-    } else if(query.back() == '?') {
-        const int firstIndex = query.find_first_of('?');
-
-        for(int i = 0; i < firstIndex; i++) {
-            if(word[i] != query[i]) {
-                return false;
+    int getChildrenCount() {
+        int count = 0;
+        for(const auto &child : children) {
+            if(child != nullptr) {
+                count++;
             }
         }
 
-        return true;
+        return count;
+    }
+};
+
+class TrieTree {
+public:
+    TrieNode *head;
+    vector<TrieNode*> nodes;
+
+    TrieTree() {
+        head = new TrieNode();
+        nodes.push_back(head);
     }
 
-    return false;
-}
+    ~TrieTree() {
+        for(int i = 0; i < nodes.size(); i++) {
+            delete nodes[i];
+        }
+    }
+
+    void insertNode(string word) {
+        const auto wordLengthMapItr = this->head->wordLengthMap.find(word.size());
+        if(wordLengthMapItr == this->head->wordLengthMap.end()) {
+            this->head->wordLengthMap[word.size()] = 1;
+        } else {
+            this->head->wordLengthMap[word.size()]++;
+        }
+
+        TrieNode *now = this->head;
+
+        for(int i = 0; i < word.size(); i++) {
+            const int index = word[i] - 'a';
+
+            if(now->children[index] == nullptr) {
+                now->children[index] = new TrieNode();
+
+                this->nodes.push_back(now->children[index]);
+
+                if(i == word.size() - 1) {
+                    now->children[index]->terminal = true;
+                }
+            }
+
+            now = now->children[index];
+
+            const auto itr = now->wordLengthMap.find(word.size());
+            if(itr == now->wordLengthMap.end()) {
+                now->wordLengthMap[word.size()] = 1;
+            } else {
+                now->wordLengthMap[word.size()]++;
+            }
+
+            if(i == word.size() - 1) {
+                now->word = true;
+            }
+        }
+    }
+
+    TrieNode* findNode(string word) {
+        TrieNode *now = this->head;
+
+        for(int i = 0; i < word.size(); i++) {
+            const int index = word[i] - 'a';
+
+            if(now->children[index] == nullptr) {
+                return nullptr;
+            }
+
+            now = now->children[index];
+        }
+
+        return now;
+    }
+};
 
 vector<int> solution(vector<string> words, vector<string> queries) {
+    set<string> wordSet;
     map<string, int> queryMap;
+    TrieTree trieTree;
+    TrieTree reversedTrieTree;
 
-    for(int i = 0; i < queries.size(); i++) {
-        if(queryMap.find(queries[i]) != queryMap.end()) {
-            continue;
-        }
+    for(const auto word : words) {
+        wordSet.insert(word);
+    }
 
-        const auto itr = queryMap.insert(make_pair(queries[i], 0)).first;
+    for(const auto word : wordSet) {
+        string reversedWord = word;
+        reverse(reversedWord.begin(), reversedWord.end());
 
-        for(int j = 0; j < words.size(); j++) {
-            if(queryWords(words[j], queries[i])) {
-                itr->second++;
-            }
-        }
+        trieTree.insertNode(word);
+        reversedTrieTree.insertNode(reversedWord);
     }
 
     vector<int> answer;
+    for(const auto query : queries) {
+        const auto queryMapItr = queryMap.find(query);
+        if(queryMapItr != queryMap.end()) {
+            answer.push_back(queryMapItr->second);
+            continue;
+        }
 
-    for(int i = 0; i < queries.size(); i++) {
-        const auto targetQuery = queryMap.find(queries[i]);
-        answer.push_back(targetQuery->second);
+        int matchCount = 0;
+        if(query.front() == '?' && query.back() == '?') {
+            const auto wordLengthMapItr = trieTree.head->wordLengthMap.find(query.size());
+            if(wordLengthMapItr != trieTree.head->wordLengthMap.end()) {
+                matchCount = wordLengthMapItr->second;
+            }
+        } else if(query.front() == '?') {
+            const int index = query.find_last_of('?') + 1;
+            string reversedQuery = query.substr(index);
+            reverse(reversedQuery.begin(), reversedQuery.end());
+
+            const TrieNode *node = reversedTrieTree.findNode(reversedQuery);
+            if(node != nullptr) {
+                const auto wordLengthMapItr = node->wordLengthMap.find(query.size());
+                if(wordLengthMapItr != node->wordLengthMap.end()) {
+                    matchCount = wordLengthMapItr->second;
+                }
+            }
+        } else if(query.back() == '?') {
+            const int index = query.find_first_of('?');
+            string targetQuery = query.substr(0, index);
+
+            const TrieNode *node = trieTree.findNode(targetQuery);
+            if(node != nullptr) {
+                const auto wordLengthMapItr = node->wordLengthMap.find(query.size());
+                if(wordLengthMapItr != node->wordLengthMap.end()) {
+                    matchCount = wordLengthMapItr->second;
+                }
+            }
+        }
+
+        queryMap.insert(make_pair(query, matchCount));
+        answer.push_back(matchCount);
     }
 
     return answer;
