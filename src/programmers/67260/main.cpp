ba8e632f1afe4d61f183d81710435d762532d0c6
youngxpepp <youngxpepp@gmail.com>
2020-09-18 21:57:07 +0900

    solve baekjoon 67260 again

--- src/programmers/67260/main.cpp
+++ src/programmers/67260/main.cpp
@@ -5,83 +5,78 @@
 
 using namespace std;
 
-class CaveTravel {
-public:
-    vector<vector<int>> graph;
-    vector<int> dfsVisit;
-    vector<bool> dfsFinished;
-    int dfsCount = 0;
-    bool cycle = false;
+bool cycle = false;
+int dfsCount = 0;
+vector<vector<int>> graph;
+vector<int> dfsVisit;
+vector<bool> dfsFinished;
 
-    CaveTravel(int n, vector<vector<int>> path, vector<vector<int>> order) {
-        this->graph.assign(n, vector<int>());
-        this->dfsVisit.assign(n, -1);
-        this->dfsFinished.assign(n, false);
+bool dfs(int nodeNumber) {
+    dfsVisit[nodeNumber] = dfsCount++;
 
-        for(int i = 0; i < order.size(); i++) {
-            graph[order[i][0]].push_back(order[i][1]);
+    bool value = true;
+
+    for(int i = 0; i < graph[nodeNumber].size(); i++) {
+        if(cycle) {
+            return false;
         }
 
-        vector<vector<int>> bidirectionalGraph(n, vector<int>());
-        for(int i = 0; i < path.size(); i++) {
-            bidirectionalGraph[path[i][0]].push_back(path[i][1]);
-            bidirectionalGraph[path[i][1]].push_back(path[i][0]);
+        int nextNodeNumber = graph[nodeNumber][i];
+
+        if(dfsVisit[nextNodeNumber] == -1) {
+            value = value & dfs(nextNodeNumber);
         }
-
-        vector<bool> bfsVisit(n, false);
-        queue<int> bfsQueue;
-        bfsQueue.push(0);
-        bfsVisit[0] = true;
-
-        while(!bfsQueue.empty()) {
-            int node = bfsQueue.front();
-            bfsQueue.pop();
-
-            for(int i = 0; i < bidirectionalGraph[node].size(); i++) {
-                if(bfsVisit[bidirectionalGraph[node][i]]) {
-                    continue;
-                }
-
-                bfsQueue.push(bidirectionalGraph[node][i]);
-                this->graph[node].push_back(bidirectionalGraph[node][i]);
-                bfsVisit[bidirectionalGraph[node][i]] = true;
-            }
+        else if(dfsVisit[nextNodeNumber] > dfsVisit[nodeNumber]) {
+            continue;
+        }
+        else if(dfsVisit[nextNodeNumber] < dfsVisit[nodeNumber] && !dfsFinished[nextNodeNumber]) {
+            cycle = true;
+            return false;
+        }
+        else {
+            continue;
         }
     }
+    dfsFinished[nodeNumber] = true;
 
-    void dfs(int node) {
-        this->dfsVisit[node] = this->dfsCount++;
-
-        for(int i = 0; i < this->graph[node].size(); i++) {
-            if(this->cycle) {
-                return;
-            }
-
-            // 트리 간선
-            if(this->dfsVisit[this->graph[node][i]] == -1) {
-                dfs(this->graph[node][i]);
-            }
-            // 순방향 간선
-            else if(this->dfsVisit[this->graph[node][i]] > this->dfsVisit[node]) {
-                continue;
-            }
-            // 역방향 간선
-            else if(this->dfsVisit[this->graph[node][i]] < this->dfsVisit[node] && !this->dfsFinished[this->graph[node][i]]) {
-                this->cycle = true;
-                return;
-            }
-            // 교차 간선
-            else {
-                continue;
-            }
-        }
-
-        this->dfsFinished[node] = true;
-    }
-};
+    return value;
+}
 
 bool solution(int n, vector<vector<int>> path, vector<vector<int>> order) {
-    CaveTravel caveTravel(n, path, order);
-    caveTravel.dfs(0);
-    return !caveTravel.cycle;
+    vector<vector<int>> biGraph(n, vector<int>());
+    graph.assign(n, vector<int>());
+
+    for(auto & v : path) {
+        biGraph[v[0]].push_back(v[1]);
+        biGraph[v[1]].push_back(v[0]);
+    }
+
+    queue<int> bfsQueue;
+    vector<bool> bfsVisit(n, false);
+    bfsQueue.push(0);
+    bfsVisit[0] = true;
+
+    while(!bfsQueue.empty()) {
+        int nodeNumber = bfsQueue.front();
+        bfsQueue.pop();
+
+        for(int i = 0; i < biGraph[nodeNumber].size(); i++) {
+            if(bfsVisit[biGraph[nodeNumber][i]]) {
+                continue;
+            }
+
+            bfsVisit[biGraph[nodeNumber][i]] = true;
+            bfsQueue.push(biGraph[nodeNumber][i]);
+            graph[nodeNumber].push_back(biGraph[nodeNumber][i]);
+        }
+    }
+
+    for(auto & v : order) {
+        graph[v[0]].push_back(v[1]);
+    }
+
+    dfsVisit.assign(n, -1);
+    dfsFinished.assign(n, false);
+
+    return dfs(0);
 }
