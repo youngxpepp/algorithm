6dacdf41d49739a6412cd009e871c42a400b4723
youngxpepp <youngxpepp@gmail.com>
2020-09-29 00:20:45 +0900

    fix: baekjoon 13460 시간 복잡도 개선
    4차원 배열 visit을 선언
    if else 대신 간결한 switch case 문 선택

--- src/baekjoon/13460/main.cpp
+++ src/baekjoon/13460/main.cpp
@@ -24,22 +24,20 @@ public:
 pair<int, int> move(const pair<int, int> &_ball, int direction) {
     pair<int, int> ball = _ball;
 
-    while (ball.first >= 0 && ball.first < table.front().size() &&
-           ball.second >= 0 && ball.second < table.size() &&
-           table[ball.second][ball.first] != '#' &&
-           table[ball.second][ball.first] != 'O') {
+    while (table[ball.second][ball.first] != 'O') {
         ball.first += directions[direction].first;
         ball.second += directions[direction].second;
-    }
 
-    if (ball.first < 0 ||
-        ball.first >= table.front().size() ||
-        ball.second < 0 ||
-        ball.second >= table.size() ||
-        table[ball.second][ball.first] == '#') {
+        if (ball.first < 0 ||
+            ball.first >= table.front().size() ||
+            ball.second < 0 ||
+            ball.second >= table.size() ||
+            table[ball.second][ball.first] == '#') {
 
-        ball.first -= directions[direction].first;
-        ball.second -= directions[direction].second;
+            ball.first -= directions[direction].first;
+            ball.second -= directions[direction].second;
+            return ball;
+        }
     }
 
     return ball;
@@ -47,80 +45,20 @@ pair<int, int> move(const pair<int, int> &_ball, int direction) {
 
 int bfs(const pair<int, int> _redBall, const pair<int, int> _blueBall) {
     queue<BfsNode> bfsQueue;
+    bool visit[10][10][10][10] = {false,};
     bfsQueue.push(BfsNode(0, _redBall, _blueBall));
 
     while (!bfsQueue.empty()) {
         BfsNode bfsNode = bfsQueue.front();
         bfsQueue.pop();
 
+        if(bfsNode.depth >= 10) {
+            continue;
+        }
+
         for (int i = 0; i < directions.size(); i++) {
-            pair<int, int> nextRedBall = bfsNode.redBall;
-            pair<int, int> nextBlueBall = bfsNode.blueBall;
-
-            pair<int, int> left = move(nextRedBall, i);
-            pair<int, int> right = move(nextBlueBall, i);
-
-            if (i == 0) {
-                if (left == right && table[left.second][left.first] != 'O') {
-                    if(nextRedBall.second > nextBlueBall.second) {
-                        nextBlueBall = move(nextBlueBall, i);
-                        nextRedBall = nextBlueBall;
-                        nextRedBall.second -= directions[i].second;
-                    } else {
-                        nextRedBall = move(nextRedBall, i);
-                        nextBlueBall = nextRedBall;
-                        nextBlueBall.second -= directions[i].second;
-                    }
-                } else {
-                    nextRedBall = move(nextRedBall, i);
-                    nextBlueBall = move(nextBlueBall, i);
-                }
-            } else if (i == 1) {
-                if (left == right && table[left.second][left.first] != 'O') {
-                    if(nextRedBall.second > nextBlueBall.second) {
-                        nextRedBall = move(nextRedBall, i);
-                        nextBlueBall = nextRedBall;
-                        nextBlueBall.second -= directions[i].second;
-                    } else {
-                        nextBlueBall = move(nextBlueBall, i);
-                        nextRedBall = nextBlueBall;
-                        nextRedBall.second -= directions[i].second;
-                    }
-                } else {
-                    nextRedBall = move(nextRedBall, i);
-                    nextBlueBall = move(nextBlueBall, i);
-                }
-            } else if (i == 2) {
-                if (left == right && table[left.second][left.first] != 'O') {
-                    if(nextRedBall.first > nextBlueBall.first) {
-                        nextBlueBall = move(nextBlueBall, i);
-                        nextRedBall = nextBlueBall;
-                        nextRedBall.first -= directions[i].first;
-                    } else {
-                        nextRedBall = move(nextRedBall, i);
-                        nextBlueBall = nextRedBall;
-                        nextBlueBall.first -= directions[i].first;
-                    }
-                } else {
-                    nextRedBall = move(nextRedBall, i);
-                    nextBlueBall = move(nextBlueBall, i);
-                }
-            } else if (i == 3) {
-                if (left == right && table[left.second][left.first] != 'O') {
-                    if(nextRedBall.first > nextBlueBall.first) {
-                        nextRedBall = move(nextRedBall, i);
-                        nextBlueBall = nextRedBall;
-                        nextBlueBall.first -= directions[i].first;
-                    } else {
-                        nextBlueBall = move(nextBlueBall, i);
-                        nextRedBall = nextBlueBall;
-                        nextRedBall.first -= directions[i].first;
-                    }
-                } else {
-                    nextRedBall = move(nextRedBall, i);
-                    nextBlueBall = move(nextBlueBall, i);
-                }
-            }
+            pair<int, int> nextRedBall = move(bfsNode.redBall, i);
+            pair<int, int> nextBlueBall = move(bfsNode.blueBall, i);
 
             if (table[nextBlueBall.second][nextBlueBall.first] == 'O') {
                 continue;
@@ -130,10 +68,29 @@ int bfs(const pair<int, int> _redBall, const pair<int, int> _blueBall) {
                 return bfsNode.depth + 1;
             }
 
-            if(bfsNode.depth >= 9) {
+            if (nextRedBall == nextBlueBall) {
+                switch (i) {
+                    case 0:
+                        bfsNode.redBall.second > bfsNode.blueBall.second ? nextRedBall.second++ : nextBlueBall.second++;
+                        break;
+                    case 1:
+                        bfsNode.redBall.second > bfsNode.blueBall.second ? nextBlueBall.second-- : nextRedBall.second--;
+                        break;
+                    case 2:
+                        bfsNode.redBall.first > bfsNode.blueBall.first ? nextRedBall.first++ : nextBlueBall.first++;
+                        break;
+                    case 3:
+                        bfsNode.redBall.first > bfsNode.blueBall.first ? nextBlueBall.first-- : nextRedBall.first--;
+                        break;
+                    default:;
+                }
+            }
+
+            if(visit[nextRedBall.second][nextRedBall.first][nextBlueBall.second][nextBlueBall.first]) {
                 continue;
             }
 
+            visit[nextRedBall.second][nextRedBall.first][nextBlueBall.second][nextBlueBall.first] = true;
             bfsQueue.emplace(bfsNode.depth + 1, nextRedBall, nextBlueBall);
         }
     }
@@ -163,13 +120,6 @@ int main() {
             }
         }
     }
-
-//    for (int i = 0; i < n; i++) {
-//        for (int j = 0; j < m; j++) {
-//            cout << table[i][j] << " ";
-//        }
-//        cout << endl;
-//    }
 
     cout << bfs(redBall, blueBall);
 
