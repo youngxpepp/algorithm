9a31f21c9bdd4c5f4ff16546f71dcfaeda43320f
youngxpepp <youngxpepp@gmail.com>
2020-12-23 00:57:55 +0900

    fix: change simple Greedy to Binary Search

--- src/baekjoon/2512/main.cpp
+++ src/baekjoon/2512/main.cpp
@@ -18,22 +18,38 @@ int main() {
     cin >> total;
     sort(arr.begin(), arr.end());
 
-    int limit = arr.back();
-    while (limit >= 0) {
+    int total_sum = 0;
+    for (int e : arr) {
+        total_sum += e;
+    }
+    if (total_sum <= total) {
+        cout << arr.back() << '\n';
+        return 0;
+    }
+
+    int answer = 0;
+    int left = 0;
+    int right = arr.back();
+    while (left <= right) {
+        int mid = (left + right) / 2;
         int sum = 0;
         for (int e : arr) {
-            if (e > limit) {
-                sum += limit;
+            if (e > mid) {
+                sum += mid;
                 continue;
             }
             sum += e;
         }
-        if (sum <= total) {
-            cout << limit << '\n';
-            break;
+
+        if (sum > total) {
+            right = mid - 1;
+        } else {
+            answer = mid;
+            left = mid + 1;
         }
-        limit--;
     }
+
+    cout << answer << '\n';
 
     return 0;
 }
